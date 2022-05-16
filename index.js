a76c404283204bee6054c2d39ea213bb6753179e
github_tuancoi20042000 <chmlys14@gmail.com.vn>
2022-05-17 01:07:20 +0700

    Inittial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+// route
+app.get('/tintuc', (req, res) => {
+    var a = 1
+    var b = 2
+    var c = a+b
+  return res.send('Hello World!')
+})
+//127.0.0.1 -localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
